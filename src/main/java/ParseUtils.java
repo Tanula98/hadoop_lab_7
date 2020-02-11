80fe5c7d14cf40effec7b62ad282d74b664db566
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 01:52:19 +0300

    Scripted auto-commit on change (2020-02-12 01:52:19) by gitwatch.sh

--- src/main/java/ParseUtils.java
+++ src/main/java/ParseUtils.java
@@ -1,2 +1,15 @@
 public class ParseUtils {
+
+    public enum CommandType {
+        GET,
+        PUT,
+        RUN_CACHE,
+        RETURN_VALUE,
+        CONNECT,
+        NOTIFY,
+        INVALID,
+        OK
+    }
+
+    
 }
