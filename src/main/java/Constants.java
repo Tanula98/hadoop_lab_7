297bb577eb048027416c7168f2c71f6676efc038
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 01:41:50 +0300

    Scripted auto-commit on change (2020-02-12 01:41:50) by gitwatch.sh

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Constants {
+    public static final String CLIENT_ROUTER_ADDRESS = "tcp://localhost:8080";
+    public static final String CACHE_ROUTER_ADDRESS = "tcp://localhost:8081";
+    public static final int HEARTBEAT_TIMEOUT = 1500;
 }
