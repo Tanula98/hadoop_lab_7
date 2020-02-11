ef6e0f118a2ec5b1b3230295a5c135776377d504
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 01:51:49 +0300

    Scripted auto-commit on change (2020-02-12 01:51:49) by gitwatch.sh

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -2,4 +2,15 @@ public class Constants {
     static final String CLIENT_ROUTER_ADDRESS = "tcp://localhost:8080";
     static final String CACHE_ROUTER_ADDRESS = "tcp://localhost:8081";
     static final int HEARTBEAT_TIMEOUT = 1500;
+
+
+    ///utiles
+    public static final Pattern GET_COMMAND_PATTERN = Pattern.compile("^GET \\d+$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern PUT_COMMAND_PATTERN = Pattern.compile("^PUT \\d+ \\d+$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern RETURN_VALUE_COMMAND_PATTERN = Pattern.compile("^RETURN_VALUE (\\d+|null)$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern CONNECT_COMMAND_PATTERN = Pattern.compile("^CONNECT \\d+ \\d+$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern NOTIFY_COMMAND_PATTERN = Pattern.compile("^NOTIFY$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern OK_COMMAND_PATTERN = Pattern.compile("^OK$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern RUN_CACHE_PATTERN = Pattern.compile("^\\d+ \\d+$");
+    public static final String DELIMITER = " ";
 }
