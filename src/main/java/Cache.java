e818b3c4d1b665acbcfb8673321f98757ee69ca8
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 01:50:45 +0300

    Scripted auto-commit on change (2020-02-12 01:50:45) by gitwatch.sh

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -1,6 +1,8 @@
 public class Cache {
 
     public static void main(String[] args) {
-        if (args.length != 2 
+        if (args.length != 2)
+           System.out.println("incorrect command-line arguments");
+            System.exit(-1);
     }
 }
