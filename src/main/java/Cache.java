73c0b1fb1306f8ce9093c40e294dc209d20ee76e
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 13:16:45 +0300

    Scripted auto-commit on change (2020-02-12 13:16:45) by gitwatch.sh

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -30,6 +30,7 @@ public class Cache {
 
             if (msg != null) {
                 String command = new String(msg.getLast().getData(), ZMQ.CHARSET);
+                ParseUtils.CommandType commandType = ParseUtils.getCommandType(command);
                 
             }
 
