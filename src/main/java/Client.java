e2ec90eff5bc52ad94521d86f86d2ca9a2238bdd
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 14:34:34 +0300

    Scripted auto-commit on change (2020-02-12 14:34:34) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,6 +4,7 @@ import org.zeromq.ZMQ;
 import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
+
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket requester = context.socket(SocketType.REQ);
         requester.connect(Constants.CLIENT_ROUTER_ADDRESS);
