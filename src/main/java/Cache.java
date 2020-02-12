4de89919534ee29bee1db57a8b3f99266699bc84
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 12:30:31 +0300

    Scripted auto-commit on change (2020-02-12 12:30:31) by gitwatch.sh

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 public class Cache {
 
     public static void main(String[] args) {
+
         if (args.length != 2 || ParseUtils.getCommandType(args[0] + " " + args[1]) != ParseUtils.CommandType.RUN_CACHE) {
             System.out.println("incorrect command-line arguments");
             System.exit(-1);
@@ -43,5 +44,5 @@ public class Cache {
     private static void sendNotifyRequest (ZMQ.Socket socket) {
         socket.send(ParseUtils.buildNotifyRequest());
     }
-    
+
 }
