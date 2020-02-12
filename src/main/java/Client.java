fca918c6bf78f3cbf4873fab8fcf9c867a629009
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 12:38:48 +0300

    Scripted auto-commit on change (2020-02-12 12:38:48) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,10 @@
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+
 public class Client {
     public static void main(String[] args) {
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket requester = context.socket(SocketType.REQ);
         
     }
 }
