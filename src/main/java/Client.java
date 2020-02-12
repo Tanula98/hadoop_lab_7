7735b552ba236496487a817ed1194adc34b33e0a
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 12:42:17 +0300

    Scripted auto-commit on change (2020-02-12 12:42:16) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -19,7 +19,7 @@ public class Client {
     }
 
     private static void ExecuteCommand(ZMQ.Socket requester, String commandLine) {
-        if (ParseUtils.getCommandType()){
+        if (ParseUtils.getCommandType(commandLine) ==){
 
         }
     }
