62ac5ebb2070c7fc100dacedfa6e3fd6b2eddcca
Tanula98 <tanula98zaa@gmail.com>
2020-02-12 13:22:13 +0300

    Scripted auto-commit on change (2020-02-12 13:22:13) by gitwatch.sh

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -53,7 +53,9 @@ public class Cache {
                 }
             }
 
-            
+            if (System.currentTimeMillis() >= nextHearbeatTime) {
+                
+            }
 
 
         }
